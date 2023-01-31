05592f6dd301d090dbb2187f1f4fd8bc185dfa0c
wangboya <wangboya.private@hotmail.com>
2023-01-31 16:01:44 +0800

    update console

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,6 +1,6 @@
 create table if not exists extensions
 (
-    name    varchar(255) not null,
+    name    varchar(255)  CHARACTER SET ascii COLLATE ascii_bin not null,
     data    longblob,
     version bigint,
     primary key (name)
